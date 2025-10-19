63efe5eff28ff40902a6a4520e2001fd693f3b4e
lam261 <caonguyenlam26@gmail.com>
2025-10-19 20:04:10 +0700

    Update Brick.java, load and render images, complete takeHit and isDestroyed logic

--- src/main/java/org/example/akranoid/Brick.java
+++ src/main/java/org/example/akranoid/Brick.java
@@ -1,4 +1,7 @@
-package org.example.akranoid;
+package org.example.baitaplon;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 /**
  * Đại diện cho viên gạch trong game.
@@ -8,11 +11,29 @@ public class Brick extends GameObject {
 
     protected int hitPoints;
     protected String type;
+    private Image image;
 
     public Brick(double x, double y, double width, double height, int hitPoints, String type) {
         super(x, y, width, height);
         this.hitPoints = hitPoints;
         this.type = type;
+        loadImageByType(type);
+    }
+
+    private void loadImageByType(String brickType) {
+        String imagePath = "/assets/blue_brick.png"; // Ảnh mặc định
+        // Ví dụ: chọn ảnh dựa vào giá trị của 'type'
+        if ("red".equalsIgnoreCase(brickType)) {
+            imagePath = "/assets/red_brick.png";
+        } else if ("green".equalsIgnoreCase(brickType)) {
+            imagePath = "/assets/green_brick.png";
+        } // Thêm các màu khác nếu có...
+
+        try {
+            this.image = new Image(getClass().getResource(imagePath).toExternalForm());
+        } catch (NullPointerException e) {
+            System.err.println("Không thể load ảnh brick type '" + brickType + "': " + e.getMessage());
+        }
     }
 
     public int getHitPoints() {
@@ -47,7 +68,18 @@ public class Brick extends GameObject {
     }
 
     @Override
-    public void render() {
-        // TODO: vẽ gạch
+    public void render(GraphicsContext gc) { // Đảm bảo có tham số gc
+        // 3. Vẽ ảnh nếu gạch chưa bị phá hủy
+        if (!isDestroyed()) {
+            if (image != null) {
+                gc.drawImage(image, x, y, width, height);
+            } else {
+                // (Tùy chọn) Vẽ hình chữ nhật màu nếu không load được ảnh
+                gc.setFill(javafx.scene.paint.Color.GRAY); // Màu mặc định
+                gc.fillRect(x, y, width, height);
+                gc.setStroke(javafx.scene.paint.Color.WHITE); // Thêm viền
+                gc.strokeRect(x, y, width, height);
+            }
+        }
     }
 }
